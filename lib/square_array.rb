47870a591b755fcf35849462bd303f357b23800f
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-12 21:55:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
-def square_array(array)
-  numbers= [" ", " "," "]
-  numbers[0]= 1**2
-  numbers[1]= 2**2
-  numbers[2]= 3**2
-  numbers
+def square_array(numbers)
+  counter= 0
+
+  while counter< number.length do
+    puts numbers[counter]**2
+    counter+= 1
+  end
 end
