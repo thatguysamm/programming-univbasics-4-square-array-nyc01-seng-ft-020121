820cd7ee6ffb65797b880e0f7cddb8b4ab887642
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-12 21:58:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   counter= 0
   square_array= []
-  while counter< number.length do
+  while counter< numbers.length do
     square_array.push(numbers[counter]**2)
     counter+= 1
   end
